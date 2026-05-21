b5ce38c3d4a5f51ff97a7eb9612828f8fac1d0a7
taurusx12 <mohanedalotibe@gmail.com>
2026-05-21 11:17:01 +0800

    Update users.js

--- src/services/users.js
+++ src/services/users.js
@@ -1,9 +1,63 @@
 const { prisma } = require('../lib/db');
+
 async function ensureUser(discordUser) {
-  return prisma.user.upsert({
-    where: { id: discordUser.id },
-    update: { username: discordUser.username },
-    create: { id: discordUser.id, username: discordUser.username }
+
+  let user = await prisma.user.findUnique({
+    where: { id: discordUser.id }
   });
+
+  if (!user) {
+    user = await prisma.user.create({
+      data: {
+        id: discordUser.id,
+        username: discordUser.username,
+        rolls: 15,
+        tokens: 0
+      }
+    });
+
+    return user;
+  }
+
+  const now = new Date();
+
+  const lastRefill = user.lastRollRefillAt || now;
+
+  const diffMs = now - lastRefill;
+
+  const hoursPassed = Math.floor(diffMs / (1000 * 60 * 60));
+
+  let newRolls = user.rolls;
+
+  if (hoursPassed > 0) {
+
+    newRolls += hoursPassed * 15;
+
+    if (newRolls > 500) {
+      newRolls = 500;
+    }
+
+    user = await prisma.user.update({
+      where: { id: discordUser.id },
+      data: {
+        username: discordUser.username,
+        rolls: newRolls,
+        lastRollRefillAt: now
+      }
+    });
+
+  } else {
+
+    user = await prisma.user.update({
+      where: { id: discordUser.id },
+      data: {
+        username: discordUser.username
+      }
+    });
+
+  }
+
+  return user;
 }
+
 module.exports = { ensureUser };
