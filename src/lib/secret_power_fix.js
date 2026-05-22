edf25091e8145d81b32264a686e64372303986a1
taurusx12 <mohanedalotibe@gmail.com>
2026-05-23 01:44:14 +0300

    Add files via upload

---
+++ src/lib/secret_power_fix.js
@@ -0,0 +1,32 @@
+// POWER BALANCE FIX
+// الشخصيات اللي كانت Divine/Mythic وصارت Secret صار لها Power عالي مناسب
+
+const SECRET_POWER_BUFFS = {
+  "Itachi": 26000,
+  "Lelouch": 24000,
+  "Chrollo": 25000,
+  "Levi": 22000,
+  "Toji": 24000,
+  "Hisoka": 22000,
+  "All Might": 25000,
+  "Kaneki": 23000,
+  "Makima": 27000,
+  "Mob": 25000,
+  "Asta": 24000,
+  "Natsu": 23000,
+  "Light Yagami": 21000,
+  "Thorfinn": 20000,
+  "Kirito": 22000,
+  "Ayanokoji": 20000,
+  "Rin Itoshi": 18000,
+  "Edward Elric": 21000,
+  "Spike Spiegel": 19000,
+  "David Martinez": 21000,
+  "Shinra Kusakabe": 23000,
+  "Esdeath": 24000,
+  "Senku": 18000,
+  "Okabe Rintarou": 18000,
+  "Mikey": 22000
+};
+
+module.exports = { SECRET_POWER_BUFFS };
